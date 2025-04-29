2d663be1c934debc03e47c59da4fdb4d61b3c7a3
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-04-29 00:41:47 -0500

    Create LevelAvg.java

---
+++ src/binary-trees-bfs/LevelAvg.java
@@ -0,0 +1,23 @@
+public class LevelAvg {
+    public List < Double > averageOfLevels(TreeNode root) {
+        List < Double > res = new ArrayList < > ();
+        Queue < TreeNode > queue = new LinkedList < > ();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            long sum = 0, count = 0;
+            Queue < TreeNode > temp = new LinkedList < > ();
+            while (!queue.isEmpty()) {
+                TreeNode n = queue.remove();
+                sum += n.val;
+                count++;
+                if (n.left != null)
+                    temp.add(n.left);
+                if (n.right != null)
+                    temp.add(n.right);
+            }
+            queue = temp;
+            res.add(sum * 1.0 / count);
+        }
+        return res;
+    }
+}
