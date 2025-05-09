acf0ac6eab87a22125a906de612df423b7b60c05
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-08 23:46:41 -0500

    Update MaxUniqueSubStrSize.java

--- src/sliding-window/MaxUniqueSubStrSize.java
+++ src/sliding-window/MaxUniqueSubStrSize.java
@@ -3,22 +3,38 @@ import java.util.Map;
 
 public class MaxUniqueSubStrSize {
 
-    // stores the index and starts the left from the duplicate+1 index
+    // same as below but faster
     public int lengthOfLongestSubstring(String s) {
-        int left=0,right;
-        int res=0;
-        Map<Character,Integer> map = new HashMap<>();
+        int n = s.length();
+             int maxLength = 0;
+             int[] lastIndex = new int[128];
+             
+             for (int start = 0, end = 0; end < n; end++) {
+                 char currentChar = s.charAt(end);
+                 start = Math.max(start, lastIndex[currentChar]);
+                 maxLength = Math.max(maxLength, end - start + 1);
+                 lastIndex[currentChar] = end + 1;
+             }
+             
+             return maxLength;
+         }
 
-        for(right=0;right<s.length();right++){
-            char rChar = s.charAt(right);
-            if(map.containsKey(rChar)){
-                left = Math.max(map.get(rChar)+1,left);
-            }
-            map.put(rChar,right);
-            res = Math.max(res,right-left+1);
-        }
-        return res;
-    }
+    // // stores the index and starts the left from the duplicate+1 index
+    // public int lengthOfLongestSubstring(String s) {
+    //     int left=0,right;
+    //     int res=0;
+    //     Map<Character,Integer> map = new HashMap<>();
+
+    //     for(right=0;right<s.length();right++){
+    //         char rChar = s.charAt(right);
+    //         if(map.containsKey(rChar)){
+    //             left = Math.max(map.get(rChar)+1,left);
+    //         }
+    //         map.put(rChar,right);
+    //         res = Math.max(res,right-left+1);
+    //     }
+    //     return res;
+    // }
 
     // iterates through till it finds the duplicate
     // public int lengthOfLongestSubstring(String s) {
