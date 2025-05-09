5293b1efa1022c2a0dc3f6081dcf54cf24aff278
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-08 23:43:40 -0500

    Update MaxUniqueSubStrSize.java

--- src/sliding-window/MaxUniqueSubStrSize.java
+++ src/sliding-window/MaxUniqueSubStrSize.java
@@ -2,6 +2,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MaxUniqueSubStrSize {
+
+    // stores the index and starts the left from the duplicate+1 index
     public int lengthOfLongestSubstring(String s) {
         int left=0,right;
         int res=0;
@@ -9,16 +11,33 @@ public class MaxUniqueSubStrSize {
 
         for(right=0;right<s.length();right++){
             char rChar = s.charAt(right);
-            map.put(rChar,map.getOrDefault(rChar,0)+1);
-            while(map.get(rChar)>1){
-                char lChar = s.charAt(left);
-                map.put(lChar,map.get(lChar)-1);
-                if(map.get(lChar)==0)
-                    map.remove(lChar);
-                left++;
+            if(map.containsKey(rChar)){
+                left = Math.max(map.get(rChar)+1,left);
             }
+            map.put(rChar,right);
             res = Math.max(res,right-left+1);
         }
         return res;
     }
+
+    // iterates through till it finds the duplicate
+    // public int lengthOfLongestSubstring(String s) {
+    //     int left=0,right;
+    //     int res=0;
+    //     Map<Character,Integer> map = new HashMap<>();
+
+    //     for(right=0;right<s.length();right++){
+    //         char rChar = s.charAt(right);
+    //         map.put(rChar,map.getOrDefault(rChar,0)+1);
+    //         while(map.get(rChar)>1){
+    //             char lChar = s.charAt(left);
+    //             map.put(lChar,map.get(lChar)-1);
+    //             if(map.get(lChar)==0)
+    //                 map.remove(lChar);
+    //             left++;
+    //         }
+    //         res = Math.max(res,right-left+1);
+    //     }
+    //     return res;
+    // }
 }
