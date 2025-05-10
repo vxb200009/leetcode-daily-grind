60196e07ca06487ee9c45ebcd04a70c7799da6bb
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-09 22:45:56 -0500

    Update O(1) space method

--- src/sliding-window/MaxOccurence1Length.java
+++ src/sliding-window/MaxOccurence1Length.java
@@ -1,3 +1,20 @@
 public class MaxOccurence1Length {
-    
+    public int longestOnes(int[] nums, int k) {
+        int start=0;
+        int maxCount=0,maxLength=0;
+        for(int end=0;end<nums.length;end++){
+           if(nums[end]==0){
+            maxCount++;
+           }
+
+           while(maxCount>k){
+            if(nums[start]==0){
+                maxCount--;
+            }
+            start++;
+           }
+           maxLength = Math.max(maxLength,end-start+1);
+        }
+        return maxLength;
+    }
 }
