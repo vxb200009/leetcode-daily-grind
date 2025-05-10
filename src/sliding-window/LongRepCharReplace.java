73083f4c2a2a40868aa6f8afb7ffec94bc4fdfd7
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-09 22:30:50 -0500

    update

--- src/sliding-window/LongRepCharReplace.java
+++ src/sliding-window/LongRepCharReplace.java
@@ -1,3 +1,32 @@
 public class LongRepCharReplace {
-    
+
+    // SLiding window Fast
+    public int characterReplacement(String s, int k) {
+        int start=0,end=0;
+        int[] frequencyMap = new int[26];
+        int maxFrequency = 0;
+        int longestSubstringLength = 0;
+
+        for(end=0;end<s.length();end++){
+            int currentChar = s.charAt(end)-'A';
+
+            frequencyMap[currentChar]+=1;
+
+            maxFrequency = Math.max(maxFrequency,frequencyMap[currentChar]);
+
+            Boolean isValid = (end+1-start-maxFrequency<=k);
+
+            if(!isValid){
+                int outGoingChar = s.charAt(start)-'A';
+                frequencyMap[outGoingChar]-=1;
+                start+=1;
+            }
+
+            longestSubstringLength = end+1-start;
+
+        }
+
+        return longestSubstringLength;
+        
+    }
 }
