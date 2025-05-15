b3c316536b93ba30d8bf74352ed11acefe90ae7c
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-14 22:37:00 -0500

    Update ConflictingAppointments.java

--- src/merge-intervals/ConflictingAppointments.java
+++ src/merge-intervals/ConflictingAppointments.java
@@ -1,4 +1,42 @@
 
 public class ConflictingAppointments {
-    
+    // only for less maxEnd value case
+    static boolean isIntersect(int[][] intervals) {
+        int n = intervals.length;
+        
+        // find the maximum end time 
+        int maxEnd = 0;
+        for (int i = 0; i < n; i++) {
+            maxEnd = Math.max(maxEnd, intervals[i][1]);
+        }
+        
+        // create an array of size maxEnd
+        // and initialize it with 0
+        int[] arr = new int[maxEnd + 2];
+        
+        // iterate over all intervals
+        for (int i = 0; i < n; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            
+            // increment the start time
+            arr[start] += 1;
+            
+            // decrement the end time
+            arr[end + 1] -= 1;
+        }
+        
+        // iterate over the array and
+        // calculate the prefix sum
+        for (int i = 1; i <= maxEnd; i++) {
+            arr[i] += arr[i - 1];
+            
+            // if the prefix sum is greater than 1
+            // then the intervals intersect
+            if (arr[i] > 1)
+                return true;
+        }
+        
+        return false;
+    }
 }
