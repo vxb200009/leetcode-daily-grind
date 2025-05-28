6d260f20b279a9c022ae3a612d638772fb8bcf23
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-27 23:50:46 -0500

    update

--- src/cyclic-sort/CyclicSort.java
+++ src/cyclic-sort/CyclicSort.java
@@ -13,4 +13,13 @@ class CyclicSort{
         for (int i = 0; i < n; i++)
             arr[i] = arr[i] / (n + 1);
     }
+
+// Example of the approach    
+ // arr = {2, 1, 5, 4, 3}
+// n = 5 → n+1 = 6
+// After first loop:
+// 	•	arr = {2, 1, 5, 4, 3}
+// 	•	Let’s go through one element:
+// arr[0] = 2 → originalVal = 2, correctIdx = 1
+// → Add 2 * 6 = 12 to arr[1], so arr[1] = 13
 }
