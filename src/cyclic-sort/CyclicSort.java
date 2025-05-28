0c8dd41cf01c2258cbbb47db9c33f73a3bbfc81a
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-27 23:46:51 -0500

    update cyclic sort

--- src/cyclic-sort/CyclicSort.java
+++ src/cyclic-sort/CyclicSort.java
@@ -1,3 +1,14 @@
 class CyclicSort{
-    
+    static void sortArr(int[] arr) {
+        int n = arr.length;
+      
+        for (int i = 0; i < n; i++) {
+            int originalVal = arr[i] % (n + 1);
+            int correctIdx = originalVal - 1;
+            arr[correctIdx] += originalVal * (n + 1);
+        }
+
+        for (int i = 0; i < n; i++)
+            arr[i] = arr[i] / (n + 1);
+    }
 }
