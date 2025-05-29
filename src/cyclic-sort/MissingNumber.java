e6bf52226425f7fccd104c50d7b4ce3d1e8b79aa
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-28 21:08:32 -0500

    update formual

--- src/cyclic-sort/MissingNumber.java
+++ src/cyclic-sort/MissingNumber.java
@@ -1,39 +1,49 @@
 class MissingNumber{
-    // add in hashSet and check till length end in order whether present in the set
+
+    // using sum of n natural numbers formula
      public int missingNumber(int[] nums) {
-        Set<Integer> numSet = new HashSet<Integer>();
-        for (int num : nums) numSet.add(num);
+        int expectedSum = nums.length*(nums.length+1)/2;
+        int actualSum = 0;
+        for(int num:nums) actualSum+=num;
 
-        int expectedNumCount = nums.length + 1;
-        for (int number = 0; number < expectedNumCount; number++) {
-            if (!numSet.contains(number)) {
-                return number;
-            }
-        }
-        return -1;
+        return expectedSum -actualSum;
+        
     }
-    // sort and find the missing easy but not efficient
-     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
+    // // add in hashSet and check till length end in order whether present in the set
+    //  public int missingNumber(int[] nums) {
+    //     Set<Integer> numSet = new HashSet<Integer>();
+    //     for (int num : nums) numSet.add(num);
 
-        // Ensure that n is at the last index
-        if (nums[nums.length-1] != nums.length) {
-            return nums.length;
-        }
-        // Ensure that 0 is at the first index
-        else if (nums[0] != 0) {
-            return 0;
-        }
+    //     int expectedNumCount = nums.length + 1;
+    //     for (int number = 0; number < expectedNumCount; number++) {
+    //         if (!numSet.contains(number)) {
+    //             return number;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    // // sort and find the missing easy but not efficient
+    //  public int missingNumber(int[] nums) {
+    //     Arrays.sort(nums);
 
-        // If we get here, then the missing number is on the range (0, n)
-        for (int i = 1; i < nums.length; i++) {
-            int expectedNum = nums[i-1] + 1;
-            if (nums[i] != expectedNum) {
-                return expectedNum;
-            }
-        }
+    //     // Ensure that n is at the last index
+    //     if (nums[nums.length-1] != nums.length) {
+    //         return nums.length;
+    //     }
+    //     // Ensure that 0 is at the first index
+    //     else if (nums[0] != 0) {
+    //         return 0;
+    //     }
 
-        // Array was not missing any numbers
-        return -1;
-    }
+    //     // If we get here, then the missing number is on the range (0, n)
+    //     for (int i = 1; i < nums.length; i++) {
+    //         int expectedNum = nums[i-1] + 1;
+    //         if (nums[i] != expectedNum) {
+    //             return expectedNum;
+    //         }
+    //     }
+
+    //     // Array was not missing any numbers
+    //     return -1;
+    // }
 }
