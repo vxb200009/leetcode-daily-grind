5b6b0694b9e5a371ca2b23325654ced038fcd887
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-05-28 22:34:02 -0500

    update

--- src/cyclic-sort/AllMissingNumbers.java
+++ src/cyclic-sort/AllMissingNumbers.java
@@ -21,29 +21,30 @@ class AllMissingNumbers{
         return result;
     }
 
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+    // using extra space faster and you can use set as well
+    // public List<Integer> findDisappearedNumbers(int[] nums) {
         
-        // Hash table for keeping track of the numbers in the array
-        // Note that we can also use a set here since we are not 
-        // really concerned with the frequency of numbers.
-        HashMap<Integer, Boolean> hashTable = new HashMap<Integer, Boolean>();
+    //     // Hash table for keeping track of the numbers in the array
+    //     // Note that we can also use a set here since we are not 
+    //     // really concerned with the frequency of numbers.
+    //     HashMap<Integer, Boolean> hashTable = new HashMap<Integer, Boolean>();
         
-        // Add each of the numbers to the hash table
-        for (int i = 0; i < nums.length; i++) {
-            hashTable.put(nums[i], true);
-        }
+    //     // Add each of the numbers to the hash table
+    //     for (int i = 0; i < nums.length; i++) {
+    //         hashTable.put(nums[i], true);
+    //     }
         
-        // Response array that would contain the missing numbers
-        List<Integer> result = new LinkedList<Integer>();
+    //     // Response array that would contain the missing numbers
+    //     List<Integer> result = new LinkedList<Integer>();
         
-        // Iterate over the numbers from 1 to N and add all those
-        // that don't appear in the hash table. 
-        for (int i = 1; i <= nums.length; i++) {
-            if (!hashTable.containsKey(i)) {
-                result.add(i);
-            }
-        }
+    //     // Iterate over the numbers from 1 to N and add all those
+    //     // that don't appear in the hash table. 
+    //     for (int i = 1; i <= nums.length; i++) {
+    //         if (!hashTable.containsKey(i)) {
+    //             result.add(i);
+    //         }
+    //     }
         
-        return result;
-    }
+    //     return result;
+    // }
 }
