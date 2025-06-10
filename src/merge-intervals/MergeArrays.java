f9a37f802f25e19a9a4cdf52586f8abe7571cd58
Venkat Kishan Bommali <36892106+vxb200009@users.noreply.github.com>
2025-06-09 21:57:10 -0500

    update

--- src/merge-intervals/MergeArrays.java
+++ src/merge-intervals/MergeArrays.java
@@ -1,3 +1,19 @@
 class MergeArrays{
-    
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] nums1Copy = new int[m];
+        for(int i=0;i<m;i++){
+            nums1Copy[i] = nums1[i]; 
+        }
+
+       int p1=0,p2=0;
+
+       for(int p=0;p<m+n;p++){
+        if(p2>=n|| p1<m && nums1Copy[p1]<nums2[p2]){
+            nums1[p]=nums1Copy[p1++];
+        }
+        else{
+            nums1[p] = nums2[p2++];
+        }
+       }
+    }
 }
